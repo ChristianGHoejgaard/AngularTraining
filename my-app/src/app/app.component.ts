b4a7a1b90398d02ff7cf130fb976de2eb0ee2ae5
Christian Højgaard Pedersen <CPEN@simcorp.com>
2026-02-20 15:18:25 +0100

    Observables and reactive programming

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./product-list/product-list.component";
 import { CopyrightDirective } from './Directives/copyright.directive';
 import { APP_SETTINGS, appSettings } from './app.settings';
+import { Observable } from 'rxjs';
+import { KeyLoggerComponent } from './key-logger/key-logger.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
+  imports: [RouterOutlet, ProductListComponent, CopyrightDirective, KeyLoggerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [ { provide: APP_SETTINGS, useValue: appSettings } ]
@@ -15,4 +17,18 @@ export class AppComponent {
   title = 'my-app';
   description = 'Hello World'
   settings = inject(APP_SETTINGS);
+  title$ = new Observable(observer => {
+    setInterval(() => {
+      observer.next(undefined);
+    }, 2000);
+  });
+
+  constructor() {
+    this.title$.subscribe(this.setTitle);
+  }
+
+  private setTitle = () => {
+    const timeStamp = new Date();
+    this.title = `${this.settings.title} (${timeStamp})`;
+  }
 }
--- my-app/src/app/product-list/product-list.component.ts
+++ my-app/src/app/product-list/product-list.component.ts
@@ -1,20 +1,20 @@
-import { OnInit, Component, inject } from '@angular/core';
+import { OnInit, Component, inject, DestroyRef } from '@angular/core';
 import { Product} from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { SortPipe } from '../Pipes/sort.pipe';
 import { ProductsService } from '../Services/products.service';
-import { FavoritesComponent } from '../favorites/favorites.component';
-import { ProductViewComponent } from '../product-view/product-view.component';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductDetailComponent, SortPipe, FavoritesComponent, ProductViewComponent],
+  imports: [ProductDetailComponent, SortPipe, AsyncPipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
   providers: [ProductsService],
 })
 export class ProductListComponent implements OnInit {
-  products: Product[] = [];
+  products$: Observable<Product[]> | undefined;
   
   selectedProduct: Product | undefined;
 
@@ -25,6 +25,10 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
+    this.getProducts();
+  }
+
+  private getProducts() {
+    this.products$ = this.productService.getProducts();
   }
 }
